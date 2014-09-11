8361b3cd3b82ff220c23d319fe0dfa70b333814f
James Spry-Leverton <james@spry-leverton.com>
2014-09-12 00:52:48 +0100

    wsgi.py sorted out

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals
 
-from werkzeug.serving import run_simple
+from manage import build_application
 
-import manage
-
-
-if __name__ == "__main__":
-    run_simple(
-        '0.0.0.0',
-        5000,
-        manage.build_application(),
-        use_reloader=True,
-        use_debugger=True
-    )
+application = build_application()
